806614c635252fabff96f3f697f3989a5df91ac1
Mayank <mayank.mishra3@globalloigc.com>
2024-04-02 22:58:08 +0530

    linkedin integration

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,27 +2,27 @@ from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+from third_parties.linkedin import scrape_linkedin_profile
 import os
 
 
 if __name__ == "__main__":
     load_dotenv()
     print("hello langchain")
-    print(os.environ["A"])
+    print(os.environ["OPENAI_API_KEY"])
     information = """"
     Elon Reeve Musk (/ˈiːlɒn/; EE-lon; born June 28, 1971) is a businessman and investor. He is the founder, chairman, CEO, and CTO of SpaceX; angel investor, CEO, product architect, and former chairman of Tesla, Inc.; owner, executive chairman, and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is one of the wealthiest people in the world, with an estimated net worth of US$190 billion as of March 2024, according to the Bloomberg Billionaires Index, and $195 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6]
     """
 
     summary_template = """
-    given the information {information}about a person I want to create:
+    given the Linkedin information {information}about a person I want to create:
     1. A short summary
     2. two interesting facts about them
     """
-
-
     summary_prompt_template = PromptTemplate(input_variables=["information"],template=summary_template)
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt= summary_prompt_template)
     res = chain.invoke(input={"information": information})
 
-    print(res)
+    linkedin_data = scrape_linkedin_profile('linkedin_profile_url')
+    print(chain.run(information=linkedin_data))
